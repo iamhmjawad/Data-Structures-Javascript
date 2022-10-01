42ad08fee8ba925cedb3c1fd2b418e11fd509423
Umair-Manzoor-47 <2020CS24@student.uet.edu.pk>
2022-10-01 21:40:42 +0500

    Added stack data structure

--- DataStructures.js
+++ DataStructures.js
@@ -5,40 +5,6 @@ class Node {
     }
   }
   
-  class Stack {
-    constructor() {
-      this.first = null;
-      this.last = null;
-      this.size = 0;
-    }
-    add(data) {
-      const newNode = new Node(data);
-      if (!this.first) {
-        this.last = newNode;
-        this.first = newNode;
-      } else {
-        const cptrFirst = this.first;
-        this.first = newNode;
-        newNode.next = cptrFirst;
-      }
-      return ++this.size;
-    }
-  
-    remove() {
-      if (!this.first) return null;
-      const cptrFirst = this.first;
-      this.first = this.first.next;
-      this.size--;
-      cptrFirst.next = null; //removing the connection, not necessary but important
-      return cptrFirst.data;
-    }
-  }
-
-  const stack = new Stack();
-  stack.add("Hello");
-  stack.add("World");
-  stack.add("!");
-  stack.remove()
   
   
   
